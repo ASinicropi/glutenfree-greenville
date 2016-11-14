b4c0982317f0c58acbc6f5e037b824e6b12e23d8
Alyssa Sinicropi <asinicropi@aim.com>
2016-11-14 18:58:11 -0500

    base layout progress

--- app/scripts/components/templates/base.jsx
+++ app/scripts/components/templates/base.jsx
@@ -6,23 +6,22 @@ function BaseLayout(props){
     <div>
       <div className="container">
         <header>
+          <ul class="nav nav-pills" id="topNav">
+            <li role="presentation"><a href="#">Sign in &amp; Sign up</a></li>
+            <li role="presentation"><a href="#">Newsletter</a></li>
+            <li role="presentation"><a href="#">Contact Us</a></li>
+          </ul>
 
+          <nav className="navbar navbar-default">
+            <div className="navbar-header">
+              <p class="navbar-text"><a href="#" class="navbar-link">Home</a></p>
+              <p class="navbar-text"><a href="#" class="navbar-link">Directory</a></p>
+              <p class="navbar-text"><a href="#" class="navbar-link">Blog</a></p>
+              <p class="navbar-text"><a href="#" class="navbar-link">About</a></p>
+              <p class="navbar-text"><a href="#" class="navbar-link">Contact</a></p>
+            </div>
+          </nav>
         </header>
-        <ul class="nav nav-pills" id="topNav">
-          <li role="presentation"><a href="#">Sign in &amp; Sign up</a></li>
-          <li role="presentation"><a href="#">Newsletter</a></li>
-          <li role="presentation"><a href="#">Contact Us</a></li>
-        </ul>
-
-        <nav className="navbar navbar-default">
-          <div className="navbar-header">
-            <p class="navbar-text"><a href="#" class="navbar-link">Home</a></p>
-            <p class="navbar-text"><a href="#" class="navbar-link">Directory</a></p>
-            <p class="navbar-text"><a href="#" class="navbar-link">Blog</a></p>
-            <p class="navbar-text"><a href="#" class="navbar-link">About</a></p>
-            <p class="navbar-text"><a href="#" class="navbar-link">Contact</a></p>
-          </div>
-        </nav>
       </div>
     </div>
   )
